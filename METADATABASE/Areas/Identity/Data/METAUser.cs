80e8f75e5af663ce4345363c461e181cd6dc7b5d
Tobi Neumann <ac117312@avcol.school.nz>
2024-03-05 11:08:35 +1300

    navbar, identity

---
+++ METADATABASE/Areas/Identity/Data/METAUser.cs
@@ -0,0 +1,6 @@
+﻿namespace METADATABASE.Areas.Identity.Data
+{
+    public class METAUser
+    {
+    }
+}
--- METADATABASE/Program.cs
+++ METADATABASE/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using METADATABASE.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Connection") ?? throw new InvalidOperationException("Connection string 'Connection' not found.");
+
+builder.Services.AddDbContext<>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
