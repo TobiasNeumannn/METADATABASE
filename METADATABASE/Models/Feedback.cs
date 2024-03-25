44e496d6c246c576e135fd1b3c980a6097790536
Tobi Neumann <ac117312@avcol.school.nz>
2024-03-26 11:10:59 +1300

    feedback table, links colours

--- METADATABASE/Models/Feedback.cs
+++ METADATABASE/Models/Feedback.cs
@@ -5,8 +5,9 @@
         public int ID { get; set; }
         public string pfp { get; set; }
         public int userID { get; set; }
-        public string desc { get; set; }
+        public string description { get; set; }
         public DateTime date { get; set; }
+        public string title { get; set; }
 
     }
 }
