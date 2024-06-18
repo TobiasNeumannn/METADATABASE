9b5f3cc096d5ccb731b29f672121d5d0b300e6a1
Tobi Neumann <ac117312@avcol.school.nz>
2024-06-19 10:10:50 +1200

    image model, scrollbar

---
+++ METADATABASE/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace METADATABASE.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+        [Column(TypeName ="nvarchar(50)")]
+        public string ImageTitle { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+    }
+}
