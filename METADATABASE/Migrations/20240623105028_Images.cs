f334e51ecfd71b3361b9a2bfd444059a2ca2ef95
Tobi Neumann <ac117312@avcol.school.nz>
2024-06-23 23:22:24 +1200

    implemented image uploading

---
+++ METADATABASE/Migrations/20240623105028_Images.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace METADATABASE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Images : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageTitle = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
--- METADATABASE/Models/ImageModel.cs
+++ METADATABASE/Models/ImageModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace METADATABASE.Models
@@ -10,6 +11,11 @@ namespace METADATABASE.Models
         [Column(TypeName ="nvarchar(50)")]
         public string ImageTitle { get; set; }
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image Name")]
         public string ImageName { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile ImageFile { get; set; }
     }
 }
